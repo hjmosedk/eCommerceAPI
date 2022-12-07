20d94323011089d5250763563887328584138dbf
Christian Kubel Højmose <hjmose@gmail.com>
2022-12-07 20:50:37 +0100

    fix: fixed CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const allowlist = ['http://192.168.1.90:3000', 'http://192.168.1.90:4200'];
+  const corsOptionsDelegate = function (req: any, callback: any) {
+    let corsOptions: any;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  };
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -15,7 +26,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors(corsOptionsDelegate);
   await app.listen(3000);
 }
 bootstrap();
