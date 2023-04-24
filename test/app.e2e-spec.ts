381d234f8e38f0b568fd101cc2550999ccd3c770
Christian Kubel Højmose <Chrisitan@hjmose.dk>
2023-04-24 12:57:37 +0200

    test(products): adjusted e2e test to test something meaningfull

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ProductsService } from '../src/products/products.service';
+import { CurrencyType, Product } from '../src/products/entities/product.entity';
+
+type newProduct = Omit<Product, 'id'>;
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let productService: ProductsService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -14,21 +19,55 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
+    productService = app.get(ProductsService);
+
+    const testProducts: newProduct[] = [
+      {
+        name: 'Diamantring',
+        sku: 'DIA1',
+        description: 'Dette er en ring af diamant',
+        category: 'Clothes',
+        price: 5000,
+        currency: CurrencyType.DKK,
+        picture: 'Not implemened',
+        quantity: 10,
+      },
+      {
+        name: 'Cheeseburger',
+        sku: 'CHE1',
+        description: 'Dette er en cheecburger',
+        category: 'Food',
+        price: 4900,
+        currency: CurrencyType.DKK,
+        picture: 'Not implemened',
+        quantity: 10,
+      },
+      {
+        name: 'Handsker',
+        sku: 'HAN1',
+        description: 'Dette er et par handsker',
+        category: 'Clothes',
+        price: 2500,
+        currency: CurrencyType.DKK,
+        picture: 'Not implemened',
+        quantity: 10,
+      },
+    ];
+
+    await productService.createMany(testProducts);
   });
 
-  test('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
   });
 
   test('/products returns a list of products', async () => {
     const products = await request(app.getHttpServer())
       .get('/products')
       .expect(200);
-
-    expect(products.body.length).toBe(0);
-    console.log(products.body);
+    console.log(
+      `This is the response from product ${JSON.stringify(products.body)}`,
+    );
+    expect(products.body.length).toBe(3);
   });
 });
