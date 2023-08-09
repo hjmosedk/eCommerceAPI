27b53496dc1e554d78c9ab68c7c2be3fd738bb86
Christian Kubel Højmose <christian@hjmose.dk>
2023-08-09 21:44:12 +0200

    chore(cleanup): cleaned up some comments

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -32,46 +32,6 @@ export class ProductsController {
     return products;
   }
 
-  /*
-  @Get('file/:name')
-  getFile(
-    @Res({ passthrough: true }) res: Response,
-    @Param('name') name: string,
-  ): StreamableFile {
-    const validateName = (name: string) => {
-      const file = name.split('.')[0];
-      const validFileName = (name: string): boolean => {
-        const uuidPattern =
-          /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
-        return uuidPattern.test(name);
-      };
-
-      if (!file) {
-        throw new BadRequestException('A file name must be provide');
-      }
-      if (!validFileName(file)) {
-        throw new BadRequestException('The file does not have a valid format');
-      }
-
-      return name;
-    };
-
-    // file deepcode ignore PT: The problem have been migrated, only valid UUDIs, can be put into the system - any UUID not in the system will throw an error
-    const imageFilePath = join(process.cwd(), `./uploads`, validateName(name));
-
-    if (!existsSync(imageFilePath)) {
-      throw new NotFoundException('No Image where stored on the server');
-    }
-
-    const image = createReadStream(imageFilePath);
-
-    res.set({
-      'Content-Type': 'application/jpeg',
-      'Content-Disposition': `attachment; filename=${name}`,
-    });
-    return new StreamableFile(image);
-  }
-*/
   @Get(':id')
   @ApiOperation({
     description: 'This will get one product from the database, based on the ID',
@@ -94,26 +54,6 @@ export class ProductsController {
     const newProduct = await this.productsService.createOne(body);
     return newProduct;
   }
-
-  /*
-  @Post('upload')
-  @UseInterceptors(
-    FileInterceptor('image', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: function (req, file, cb) {
-          const mimeType = file.mimetype.split('/')[1];
-          const fileID = randomUUID();
-          const fileName = `${fileID}.${mimeType}`;
-          cb(null, fileName);
-        },
-      }),
-    }),
-  )
-  uploadFile(@UploadedFile() image: Express.Multer.File) {
-    return { name: image.filename };
-  }
-*/
 
   @Patch(':id')
   @ApiOperation({
