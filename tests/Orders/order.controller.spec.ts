8e3f2828e882288b49c339e1d494135fc4ac2b64
Christian Kubel Højmose <christian@hjmose.dk>
2023-10-03 20:33:27 +0200

    test(orders): moved the test to align with other tests

--- tests/Orders/order.controller.spec.ts
+++ tests/Orders/order.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OrderController } from './order.controller';
+import { OrderController } from '../../src/orders/order.controller';
 
 describe('OrderController', () => {
   let controller: OrderController;
